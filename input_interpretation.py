da05f653f9778e1adb68f7d04dd029642b614dbd
Sebastian Krautwurst <raverjay@gmail.com>
2020-11-04 17:30:08 +0100

    logic fix

--- input_interpretation.py
+++ input_interpretation.py
@@ -36,7 +36,7 @@ def parse_args():
 
 def check_input(args):
     bam_files = []
-    for bam_file in args.bam_files:
+    for bam_file in args.bamfile:
         if not os.path.isfile(bam_file):
             error(f'Not a file: {bam_file}')
         if bam_file in bam_files:
@@ -47,8 +47,11 @@ def check_input(args):
                 f'Bam index missing for file: {bam_file}. Trying "samtools index {bam_file}" ...')
             ret = os.system(f'samtools index {bam_file}')
             if ret != 0:
-                log(
-                    f'Warning: samtools index returned exit code {ret} - everything might crash and burn.')
+                error(
+                    f'ERROR: samtools index returned exit code {ret}')
+        # checked
+        bam_files.append(bam_file)
+        
     num_bam_files = len(bam_files)
     log(f'Number of .bam files: {num_bam_files}')
     return bam_files, num_bam_files
